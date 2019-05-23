a2f47b59786028f828d378f4fb0b57069fd6fb81
pontura <pontura@gmail.com>
2019-05-23 12:21:54 -0300

    ui

---
+++ reloj/Assets/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Image backgroundImage;
+    public Color color;
+    public GameObject ui;
+    public bool isUIOpened;
+
+    void Start()
+    {
+        ui.SetActive(false);
+    }
+    void Update()
+    {
+        backgroundImage.color = color;
+    }
+    public void OnButtonClick()
+    {
+        if(isUIOpened)
+        {
+            ui.SetActive(false);
+            isUIOpened = false;
+        }
+        else
+        {            
+            ui.SetActive(true);
+            isUIOpened = true;
+        }
+    }
+}
