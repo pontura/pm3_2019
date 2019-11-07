315ebd222b37f843d6c632f0473eebc6abf975bd
pontura <pontura@gmail.com>
2019-11-07 11:29:17 -0300

    json

---
+++ videogame/Assets/ServerLoader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class ServerLoader : MonoBehaviour
+{
+    public DataLoaded dataLoaded;
+    [Serializable]
+    public class DataLoaded
+    {
+        public List<TutorialData> content;
+    }
+    private void Start()
+    {
+        StartCoroutine(LoadData());
+    }
+    IEnumerator LoadData()
+    { 
+        string url = "http://niitek.com.ar/api/tutorial.php";
+        WWW www = new WWW(url);
+
+        yield return www;
+        if (www.error == null)
+        {
+            Processjson(www.text);
+        }
+        else
+        {
+            Debug.Log("ERROR: " + www.error);
+        }
+    }
+    void Processjson(string data)
+    {
+        print(data);
+        string d = "{\"content\":" + data + "}";
+        print(d);
+        dataLoaded = JsonUtility.FromJson<DataLoaded>(d);
+
+    }
+}
---
+++ videogame/Assets/TutorialData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class TutorialData
+{
+    public string username;
+    public string title;
+    public string content;
+    public string timestamp;
+    public string banner;
+    public string summary;
+}
